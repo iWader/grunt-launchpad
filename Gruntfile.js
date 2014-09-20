eac1d5b82b8e64f4897c5044ca5e168ef4850e12
Wade Urry <wader@iwader.co.uk>
2014-09-20 14:45:39 +0100

    Load all the grunt tasks using magics

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,7 @@
 
 module.exports = function(grunt) {
 
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+    require('load-grunt-tasks')(grunt);
 
     var config = {
         bower_path: 'bower_components',
